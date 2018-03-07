599d9b125ec7e25ff3cb0f674ede19a1bab1eed3
RICK HOPKINS <rhopkins@nhaschools.com>
2018-03-07 14:11:32 -0500

    Reworked scrolling again

--- grid.js
+++ grid.js
@@ -1,45 +1,33 @@
 /** initialize the grid */
 (() => {
 	/** get the grid pieces */
-	const gridDataArea = document.querySelector('.data-area');
 	const gridDataContainer = document.querySelector('.data-container');
 	const gridHeaderContainer = document.querySelector('.header-container');
-	const gridDataScroller = document.querySelector('.data-container-scroller');
-	const gridDataScrollerSpacer = document.querySelector('.data-container-scroller > .spacer');
+	const gridLeftDataColumn = document.querySelector('.data-area .left-pinned-columns');
 
-	/** set the width of the data container scroller spacer */
-	gridDataScrollerSpacer.style.width = (gridHeaderContainer.scrollWidth - 25) + 'px';
-
-	/** create flag to track horizontal scrolling */
-	let isScrolling;
+	/** track the active side */
+	let leftActive = false;
 
 	/** add the scroll event listeners */
-	gridDataArea.addEventListener('scroll', () => {
-		/** check if the scrollTop + gridDataArea height is equal to scrollHeight */
-		const bounds = gridDataArea.getClientRects()[0];
-		const position = gridDataArea.scrollTop + bounds.height;
-		if (position === gridDataArea.scrollHeight) {
-			gridDataScroller.style.display = 'none';
-		} else {
-			gridDataScroller.style.display = 'block';
-		}
-
-		/** sync scrollbars */
-		syncScrollLeft(gridDataContainer, gridDataScroller);
-	});
 	gridDataContainer.addEventListener('scroll', () => {
 		/** scroll the other grid pieces */
 		gridHeaderContainer.scrollLeft = gridDataContainer.scrollLeft;
-		gridDataScroller.scrollLeft = gridDataContainer.scrollLeft;
+		if (!leftActive) {
+			gridLeftDataColumn.scrollTop = gridDataContainer.scrollTop;
+		}
 	}, { passive: true });
-	gridDataScroller.addEventListener('scroll', () => {
-		/** clear the isScrolling timeout and reset */
-		clearTimeout(isScrolling);
-		isScrolling = setTimeout(syncScrollLeft.bind(null, gridDataScroller, gridDataContainer), 10);
+	gridLeftDataColumn.addEventListener('scroll', () => {
+		/** scroll the other grid pieces */
+		if (leftActive) {
+			gridDataContainer.scrollTop = gridLeftDataColumn.scrollTop;
+		}
 	}, { passive: true });
 
-	/** sync the scrollLeft values */
-	const syncScrollLeft = (primary, secondary) => {
-		secondary.scrollLeft = primary.scrollLeft;
-	};
+	/** add mouseover event listeners */
+	gridDataContainer.addEventListener('mouseover', () => {
+		leftActive = false;
+	}, { passive: true });
+	gridLeftDataColumn.addEventListener('mouseover', () => {
+		leftActive = true;
+	}, { passive: true });
 })();
