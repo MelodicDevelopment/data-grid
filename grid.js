8aec40c212835d7001f8cd40477d385751f78784
Rick Hopkins <rick.hopkins@gmail.com>
2018-03-08 08:30:21 -0500

    yet more scroll changes

--- grid.js
+++ grid.js
@@ -3,31 +3,10 @@
 	/** get the grid pieces */
 	const gridDataContainer = document.querySelector('.data-container');
 	const gridHeaderContainer = document.querySelector('.header-container');
-	const gridLeftDataColumn = document.querySelector('.data-area .left-pinned-columns');
-
-	/** track the active side */
-	let leftActive = false;
 
 	/** add the scroll event listeners */
 	gridDataContainer.addEventListener('scroll', () => {
 		/** scroll the other grid pieces */
 		gridHeaderContainer.scrollLeft = gridDataContainer.scrollLeft;
-		if (!leftActive) {
-			gridLeftDataColumn.scrollTop = gridDataContainer.scrollTop;
-		}
-	}, { passive: true });
-	gridLeftDataColumn.addEventListener('scroll', () => {
-		/** scroll the other grid pieces */
-		if (leftActive) {
-			gridDataContainer.scrollTop = gridLeftDataColumn.scrollTop;
-		}
-	}, { passive: true });
-
-	/** add mouseover event listeners */
-	gridDataContainer.addEventListener('mouseover', () => {
-		leftActive = false;
-	}, { passive: true });
-	gridLeftDataColumn.addEventListener('mouseover', () => {
-		leftActive = true;
 	}, { passive: true });
 })();
