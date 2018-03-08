67ecac663c418d172c28ac075f403abd6f5a4532
RICK HOPKINS <rhopkins@nhaschools.com>
2018-03-08 09:01:22 -0500

    Yet another... maybe the last. We'll see. Scroll update

--- grid.js
+++ grid.js
@@ -7,10 +7,23 @@
 
 	/** add the scroll event listeners */
 	gridDataArea.addEventListener('scroll', () => {
-		gridDataContainer.style.bottom = `${gridDataArea.scrollTop * -1}px`;
+		adjustGridArea();
 	}, { passive: true });
 	gridDataContainer.addEventListener('scroll', () => {
 		/** scroll the other grid pieces */
 		gridHeaderContainer.scrollLeft = gridDataContainer.scrollLeft;
 	}, { passive: true });
+
+	/** watch for windows resizes */
+	window.addEventListener('resize', () => {
+		/** adjust the bottom for the data container */
+		gridDataContainer.style.bottom = '0px';
+		adjustGridArea();
+	});
+
+	/** adjust the bottom of the grid area */
+	const adjustGridArea = () => {
+		/** adjust the bottom for the data container */
+		gridDataContainer.style.bottom = `${gridDataArea.scrollTop * -1}px`;
+	};
 })();
