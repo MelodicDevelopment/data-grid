717715bdf6e4b3da025c476fdbe86a824d38df59
Rick Hopkins <rick.hopkins@gmail.com>
2018-03-08 08:36:32 -0500

    Another scroll change

--- grid.js
+++ grid.js
@@ -1,10 +1,14 @@
 /** initialize the grid */
 (() => {
 	/** get the grid pieces */
+	const gridDataArea = document.querySelector('.data-area');
 	const gridDataContainer = document.querySelector('.data-container');
 	const gridHeaderContainer = document.querySelector('.header-container');
 
 	/** add the scroll event listeners */
+	gridDataArea.addEventListener('scroll', () => {
+		gridDataContainer.style.bottom = `${gridDataArea.scrollTop * -1}px`;
+	}, { passive: true });
 	gridDataContainer.addEventListener('scroll', () => {
 		/** scroll the other grid pieces */
 		gridHeaderContainer.scrollLeft = gridDataContainer.scrollLeft;
