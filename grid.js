418809f444219bb3b24c263efffbf33a626a09ef
Rick Hopkins <rick.hopkins@gmail.com>
2018-11-23 21:35:04 -0500

    Add cell activation

--- grid.js
+++ grid.js
@@ -1,5 +1,11 @@
-/** initialize the grid */
-(() => {
+/** grid properties */
+const gridProperties = {
+	activeCell: -1,
+	activeRow: -1
+};
+
+/** intialize the grid */
+const init = () => {
 	/** get the grid pieces */
 	const gridDataArea = document.querySelector('.data-area');
 	const gridDataContainer = document.querySelector('.data-area .unpinned-column-group');
@@ -32,4 +38,126 @@
 		/** adjust the bottom for the data container */
 		gridDataContainer.style.bottom = `${bottom}px`;
 	};
+};
+
+/** add click ability to all cells */
+const configureCells = () => {
+	/** get the cells */
+	const pinnedDataRows = Array.from(document.querySelectorAll('.data-area .pinned-column-group .data-row'));
+	const unPinnedDataRows = Array.from(document.querySelectorAll('.data-area .unpinned-column-group .data-row'));
+
+	/** create the data rows */
+	const dataRows = [];
+	pinnedDataRows.forEach((pdr, i) => {
+		/** create cell array */
+		const dataRowCells = [].concat(
+			Array.from(pdr.querySelectorAll('.cell')),
+			Array.from(unPinnedDataRows[i].querySelectorAll('.cell'))
+		);
+		
+		/** add the data row */
+		dataRows.push(dataRowCells);
+	});
+
+	/** cycle through rows */
+	dataRows.forEach((row, i) => {
+		/** cycle through cells */
+		row.forEach((cell, j) => {
+			/** add cell properties */
+			cell.setAttribute('tabindex', 0);
+			cell.setAttribute('row-index', i);
+			cell.setAttribute('cell-index', j);
+
+			/** add click event */
+			cell.addEventListener('click', () => {
+				/** set active state */
+				removeActiveCellState();
+				cell.classList.add('active');
+
+				/** set the grid active row and cell */
+				gridProperties.activeRow = parseInt(cell.getAttribute('row-index'));
+				gridProperties.activeCell = parseInt(cell.getAttribute('cell-index'));
+
+				/** scroll the cell into view */
+				scrollIntoView(cell);
+			});
+
+			/** add keyup event */
+			cell.addEventListener('keyup', (event) => {
+				/** remove the active cell state */
+				removeActiveCellState();
+
+				/** set the active row and cell */
+				if (event.keyCode === 38 && gridProperties.activeRow != 0) gridProperties.activeRow--; // up
+				if (event.keyCode === 40 && gridProperties.activeRow < (dataRows.length - 1)) gridProperties.activeRow++; // down
+				if (event.keyCode === 37 && gridProperties.activeCell != 0) gridProperties.activeCell--; // left
+				if (event.keyCode === 39 && gridProperties.activeCell < (row.length - 1)) gridProperties.activeCell++; // right
+
+				/** find the active cell */
+				const newActiveCell = document.querySelector(`.data-area .cell[row-index="${gridProperties.activeRow}"][cell-index="${gridProperties.activeCell}"]`);
+				newActiveCell.classList.add('active');
+
+				/** scroll the cell into view */
+				scrollIntoView(newActiveCell);
+			});
+
+			/** kill keydown event to prevent scrolling */
+			cell.addEventListener('keydown', (event) => {
+				event.preventDefault();
+				event.stopPropagation();
+			});
+		});
+	});
+};
+
+/** remove active class from all cells */
+const removeActiveCellState = () => {
+	const allCells = Array.from(document.querySelectorAll('.data-area .cell'));
+	allCells.forEach(c => c.classList.remove('active'));
+};
+
+/** scroll cell into view */
+const scrollIntoView = (cell) => {
+	/** get the scroll areas */
+	const gridDataArea = cell.closest('.data-area');
+	const gridDataUnpinned = cell.closest('.unpinned-column-group');
+
+	/** get the bounds of the cell and */
+	const gridDataCoords = gridDataArea.getBoundingClientRect();
+	const cellCoords = cell.getBoundingClientRect();
+
+	/** check if we're out of view horizontally */
+	if (gridDataUnpinned) {
+		/** get the unpinned coords */
+		const unPinnedCoords = gridDataUnpinned.getBoundingClientRect();
+
+		/** scroll horizontally */
+		if (cellCoords.left < unPinnedCoords.left) {
+			const diff = (unPinnedCoords.left - cellCoords.left) * -1;
+			gridDataUnpinned.scrollBy(diff, 0);
+		}
+		if (cellCoords.right > unPinnedCoords.right) {
+			const diff = cellCoords.right - unPinnedCoords.right;
+			gridDataUnpinned.scrollBy(diff, 0);
+		}
+	}
+
+	/** check if we need to scroll vertically */
+	if (cellCoords.top < gridDataCoords.top) {
+		const diff = (gridDataCoords.top - cellCoords.top + 1) * -1;
+		gridDataArea.scrollBy(0, diff);
+	}
+	if ((cellCoords.bottom + 18) > gridDataCoords.bottom) {
+		const diff = (cellCoords.bottom - gridDataCoords.bottom) + 18;
+		gridDataArea.scrollBy(0, diff);
+	}
+};
+
+/** initialize the grid */
+(() => {
+	/** initialize the grid */
+	init();
+
+	/** configure the cells */
+	configureCells();
 })();
